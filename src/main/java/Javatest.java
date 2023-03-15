99436c12ae6e8f86b081684f1a409e71961ccca3
JeongukChoi <onsucjuk@gmail.com>
2023-03-15 11:00:53 +0900

    bj no.2439 exam

---
+++ src/main/java/Javatest.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class Javatest {
+
+    public static void main(String[] args) {
+
+        Scanner ad = new Scanner(System.in);
+
+        int a = ad.nextInt();
+
+        for (int b = 0; b < a; b++) {
+
+            for (int c = a; c-1 > b; c--) {
+
+                System.out.print(" ");
+
+            }
+
+            for (int d = 0; d <= b; d++) {
+
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
+
+
