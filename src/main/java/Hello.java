5b7ff76f423d2012955763f8975cf2f2fa6c05cb
JeongukChoi <onsucjuk@gmail.com>
2023-03-15 09:34:05 +0900

    수정 테스트

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,22 +1,11 @@
-import java.util.*;
-
-
 public class Hello {
 
     public static void main(String[] args){
 
-        Scanner ad = new Scanner(System.in);
-
-        int a = ad.nextInt();
-        int[] b = new int[a];
-        int sum = 0;
-        for (int c = 0; c<a ; c++) {
-
-           b[c] = a-c;
-           sum += b[c];
-        }
-
-        System.out.println(sum);
+        System.out.println("Hello!! My First Java Coding!!");
+        System.out.println("자바 공부");
+        System.out.println("코딩 like");
+        System.out.println("수정 테스트");
 
     }
 
