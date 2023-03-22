0c75182b4f4436560600a3b00845e2b9c7da481b
JeongukChoi <onsucjuk@gmail.com>
2023-03-22 10:24:57 +0900

    Class Ex

--- src/main/java/Ex4_4.java
+++ src/main/java/Ex4_4.java
@@ -3,12 +3,12 @@ public class Ex4_4 {
 
         int a = 10, b;
 
-        b = a++;
+        b = a++; // 선대입 출력 후 연산 ++
 
-        System.out.printf("%d \n",b); // 선대입 출력 후 연산 ++
+        System.out.printf("%d \n",b);
 
-        b = ++a;
-        System.out.printf("%d \n",b); // 선++연산 후 대입출력
+        b = ++a; // 선++연산 후 대입출력
+        System.out.printf("%d \n",b);
 
     }
 
