b51a3980e0855a03bd3188f3c407a717bf87058e
JeongukChoi <onsucjuk@gmail.com>
2023-03-22 10:47:26 +0900

    Class Ex

---
+++ src/main/java/Ex4_5.java
@@ -0,0 +1,17 @@
+public class Ex4_5 {
+    public static void main(String[] args) {
+
+        int a = 100, b = 200;
+
+        System.out.printf(" %d == %d 는 %s 이다. \n", a, b, a == b);
+        System.out.printf(" %d != %d 는 %s 이다. \n", a, b, a != b);
+        System.out.printf(" %d > %d 는 %s 이다. \n", a, b, a >b);
+        System.out.printf(" %d < %d 는 %s 이다. \n", a, b, a < b);
+        System.out.printf(" %d >= %d 는 %s 이다. \n", a, b, a >= b);
+        System.out.printf(" %d <= %d 는 %s 이다. \n", a, b, a <= b);
+
+        System.out.printf(" %d = %d 는 %s 이다. \n", a, b, a =b);
+
+    }
+
+}
