9c7fca7c20a08c763eb5f7d8f158e5fa4c342c34
JeongukChoi <onsucjuk@gmail.com>
2023-03-22 09:34:56 +0900

    Class Ex

---
+++ src/main/java/Ex03_01.java
@@ -0,0 +1,13 @@
+public class Ex03_01 {
+    public static void main(String[] args){
+
+        System.out.printf("100+100"); // 100+100 모두 문자
+        System.out.printf("\n"); // 강제로 행 넘기기
+        System.out.printf("%d", 100+100); // 숫자로 출력
+        System.out.printf("\n"); // 강제로 행 넘기기
+
+
+
+    }
+
+    }
