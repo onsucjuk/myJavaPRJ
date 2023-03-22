6220fac55db31eaf27884a84a28f21baff1634a9
JeongukChoi <onsucjuk@gmail.com>
2023-03-22 10:18:17 +0900

    Class Ex

---
+++ src/main/java/Ex4_3.java
@@ -0,0 +1,35 @@
+public class Ex4_3 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        a++;
+        System.out.printf(" a ++ ==> %d \n", a);
+
+        a--;
+
+        System.out.printf(" b -- ==> %d \n", a);
+
+        a += 5;
+
+        System.out.printf(" a += 5 ==> %d \n", a);
+
+        a -= 5;
+
+        System.out.printf(" a -= 5 ==> %d \n", a);
+
+        a *= 5;
+
+        System.out.printf(" a *= 5 ==> %d \n", a);
+
+        a /= 5;
+
+        System.out.printf(" a /= 5 ==> %d \n", a);
+
+        a %= 5;
+
+        System.out.printf(" a %%= 5 ==> %d \n", a);
+
+    }
+}
---
+++ src/main/java/Ex4_4.java
@@ -0,0 +1,15 @@
+public class Ex4_4 {
+    public static void main(String[] args) {
+
+        int a = 10, b;
+
+        b = a++;
+
+        System.out.printf("%d \n",b);
+
+        b = ++a;
+        System.out.printf("%d \n",b);
+
+    }
+
+}
