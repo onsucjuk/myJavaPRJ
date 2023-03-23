3cd2a37981816b7e3f11d7697d83228f0311726b
JeongukChoi <onsucjuk@gmail.com>
2023-03-23 14:02:48 +0900

    Ex add

--- src/main/java/Javatest.java
+++ src/main/java/Javatest.java
@@ -1,29 +1,28 @@
 import java.util.Scanner;
-
-
 public class Javatest {
-
     public static void main(String[] args) {
-
+        System.out.printf("입력 값 : ");
         Scanner ad = new Scanner(System.in);
-
         int a = ad.nextInt();
 
-        for (int b = 0; b < a; b++) {
+        if (a%3==0 && a%5==0) {
+            System.out.printf("출력결과\n");
+            System.out.printf("3의 배수와 5의 배수를 둘다 만족합니다. \n");
 
-            for (int c = a; c-1 > b; c--) {
+        } else if(a%3==0) {
 
-                System.out.print(" ");
+            System.out.printf("출력결과\n");
+            System.out.printf("3의 배수입니다. \n");
 
-            }
+        } else if(a%5==0) {
 
-            for (int d = 0; d <= b; d++) {
+            System.out.printf("출력결과\n");
+            System.out.printf("5의 배수입니다. \n");
 
-                System.out.print("*");
+        } else {
 
-            }
-            System.out.println(" ");
-
+            System.out.printf("출력결과\n");
+            System.out.printf("3의 배수도 5의 배수도 아닙니다. \n");
         }
     }
 }
