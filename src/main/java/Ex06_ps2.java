0e772446ff11d8e47919625959057e2995925e7d
JeongukChoi <onsucjuk@gmail.com>
2023-03-31 07:41:25 +0900

    Ex only one while, 9x9

---
+++ src/main/java/Ex06_ps2.java
@@ -0,0 +1,29 @@
+public class Ex06_ps2 {
+    public static void main(String[] args) {
+
+        int i = 1;
+        int j = 2;
+
+        while (j < 10) {
+
+            if (j<10) {
+
+                System.out.printf(" %d x %d = %d ", j, i, j * i);
+
+                if (i < 9) {
+
+                    i++;
+
+                } else {
+
+                    i = 1;
+                    ++j;
+                    System.out.printf("\n");
+
+                }
+            }
+
+        }
+
+    }
+}
