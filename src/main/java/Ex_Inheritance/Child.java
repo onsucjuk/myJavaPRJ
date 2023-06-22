c528cf16d75c190cb5f22a748f79f0874b2b67c9
JeongukChoi <onsucjuk@gmail.com>
2023-06-23 08:13:22 +0900

    0623 잔디 부모 자식 상속 extends

---
+++ src/main/java/Ex_Inheritance/Child.java
@@ -0,0 +1,9 @@
+package Ex_Inheritance;
+
+public class Child extends Parent {
+
+    @Override
+    public int cal(int a, int b){
+        return a * b;
+    }
+}
---
+++ src/main/java/Ex_Inheritance/Parent.java
@@ -0,0 +1,7 @@
+package Ex_Inheritance;
+
+public class Parent {
+    public int cal(int a, int b) {
+            return a + b;
+    }
+}
